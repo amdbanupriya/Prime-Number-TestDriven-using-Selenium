c18f49737f702cd040f6bdcbbdfbc43888aebd33
amdbanupriya <37889545+amdbanupriya@users.noreply.github.com>
2018-03-29 18:24:58 -0400

    Add files via upload

---
+++ Source Code/PrimeNumberGenerater.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PrimeNumberGenerater
+{
+    interface PrimeNumberGenerater
+    {
+        List<int> generate(int StartNumber, int EndNumber);
+        bool isPrime(int Value);
+    }
+    class PrimeNumber : PrimeNumberGenerater
+    {
+        public bool isPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+        public List<int> generate(int StartNumber, int EndNumber)
+        {
+            List<int> result = new List<int>();
+            int i = 0;
+            if (StartNumber <= EndNumber)
+            {
+                for (i = StartNumber; i <= EndNumber; i++)
+                {
+                    if (isPrime(i))
+                        result.Add(i);
+                }
+            }
+            else
+            {
+                for (i = StartNumber; i >= EndNumber; i--)
+                {
+                    if (isPrime(i))
+                        result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("||***Welcome to Prime Number Generator***||");
+    //        Console.WriteLine("Enter start number(numeric value):");
+    //        string num1 = (Console.ReadLine());
+    //        Console.WriteLine("Enter end number(numeric value):");
+    //        string num2 = (Console.ReadLine());
+    //        if (Regex.IsMatch(num1, @"^[0-9]+$") && Regex.IsMatch(num2, @"^[0-9]+$"))
+    //        {
+    //            PrimeNumber obj = new PrimeNumber();
+    //            if(obj.generate(Convert.ToInt32(num1), Convert.ToInt32(num2)).Count == 0)
+    //                Console.WriteLine("No Prime Numbers between " + num1 + " to " + num2);
+    //            else
+    //                Console.WriteLine("Prime Numbers between " + num1 + " to " + num2 + " = " + string.Join(",", obj.generate(Convert.ToInt32(num1), Convert.ToInt32(num2))));
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Enter the valid numeric input!!!");
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+}
---
+++ Source Code/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using excel = Microsoft.Office.Interop.Excel;
+using PrimeNumberGenerater;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace PrimeNumberTestDrivenusingSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("||***Welcome to Test Driven Prime Number Generator***||");
+            try
+            {
+                if (File.Exists(@"C:\PrimeNumberTestDrivenusingSelenium\TestCase_Input.xlsx"))
+                {
+                    excel.Application x1Appl = new excel.Application();
+                    excel.Workbook x1WorkBook = x1Appl.Workbooks.Open(@"C:\PrimeNumberTestDrivenusingSelenium\TestCase_Input.xlsx");
+                    excel._Worksheet x1WorkSheet = x1WorkBook.Sheets[1];
+                    excel.Range x1Range = x1WorkSheet.UsedRange;
+                    PrimeNumber objPrimeNumber = new PrimeNumber();
+                    for (int i = 2; i <= Convert.ToInt32(x1Range.Rows.Cells[8, 3].value) + 1; i++)
+                    {
+                        if (CheckPrimeNumber(x1Range.Rows.Cells[i, 2].Text, x1Range.Rows.Cells[i, 3].Text) == x1Range.Rows.Cells[i, 4].Text)
+                        {
+                            x1Range.Rows.Cells[i, 5].value = "PASS";
+                            x1Range.Rows.Cells[i, 6].value = CheckPrimeNumber(x1Range.Rows.Cells[i, 2].Text, x1Range.Rows.Cells[i, 3].Text);
+                        }
+                        else
+                        {
+                            x1Range.Rows.Cells[i, 5].value = "FAIL";
+                            x1Range.Rows.Cells[i, 6].value = CheckPrimeNumber(x1Range.Rows.Cells[i, 2].Text, x1Range.Rows.Cells[i, 3].Text);
+                        }
+                    }
+                    x1Appl.DisplayAlerts = false;
+                    x1WorkBook.SaveAs(string.Format(@"C:\PrimeNumberTestDrivenusingSelenium\TestCase_Result_{0:MM-dd-yyyy_hh-mm-ss-tt}.xlsx", DateTime.Now));
+                    x1WorkBook.Close(0, 0, 0);
+                    x1Appl.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(x1Appl);
+                    Console.WriteLine(string.Format("Test execution completed.Please refer the file TestCase_Result_{0:MM-dd-yyyy_hh-mm-ss-tt}.xlsx", DateTime.Now));
+                }
+                else
+                    Console.WriteLine("TestCase_Input.xlxs file not exists");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(string.Concat("Exception: ",ex.Data,".Please contact admin"));
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+
+        static string CheckPrimeNumber(string startNumber, string endNumber)
+        {
+            if (Regex.IsMatch(startNumber, @"^[0-9]+$") && Regex.IsMatch(endNumber, @"^[0-9]+$"))
+            {
+                PrimeNumber obj = new PrimeNumber();
+                List<int> result = obj.generate(Convert.ToInt32(startNumber), Convert.ToInt32(endNumber));
+                if (result.Count == 0)
+                    return "No Prime Numbers";
+                else
+                    return string.Join(",", result);
+            }
+            else
+            {
+                return "Invalid input";
+            }
+        }
+    }
+}
